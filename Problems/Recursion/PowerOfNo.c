2a376b70f50d06ce8cc7f36deab4d0dfbad42a66
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2021-10-30 21:04:34 +0530

    Update PowerOfNo.c

--- Problems/Recursion/PowerOfNo.c
+++ Problems/Recursion/PowerOfNo.c
@@ -2,32 +2,32 @@
 
 int f(int x, int k)
 {
-	
+	int a,b;
 	if(k==0)
 	{
-		return 1;
+	    return 1;
 	}
-	
-
-	return f(x,k-1)*x;
-	
+	a=f(x,k-1);
+        b=a*x;
+	return b;
 }
 int main()
 {
 	int x=2;
 	int k=5;
 	int i;
-	
 	printf("f(%d,%d) =",x,k);
 	for(i=1; i<=k; i++)
 	{
 		printf(" %d ",x);
-		
 	}
-	printf("\n");
+    printf("\n");
     i= f(x,k);
     printf("%d ",i);
 }
+
+
+
 
 
 #OUTPUT:
